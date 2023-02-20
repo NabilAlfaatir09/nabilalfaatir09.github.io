41cd0d06ccea6e147978651c81762f0e633f2f20
Nabil Alfaatir <nabil_alfaatir09@yahoo.com>
2023-02-20 23:04:10 +0700

    selesai web di tampilan laptop

---
+++ script.js
@@ -0,0 +1,28 @@
+const nav = document.querySelector(".navbar");
+document.querySelector("#hamburger-menu").onclick = () => {
+    nav.classList.toggle("active");
+}
+
+const hamburger = document.querySelector("#hamburger-menu");
+document.addEventListener("click", function (e) {
+    if (!hamburger.contains(e.target) && !nav.contains(e.target)) {
+        nav.classList.remove("active");
+    }
+});
+
+function validation() {
+    const nama = document.forms["form-contact"]["nama"].value;
+    const tglLahir = document.forms["form-contact"]["tanggal-lahir"].value;
+    const gender = document.forms["form-contact"]["gender"].value;
+    const pesan = document.forms["form-contact"]["pesan"].value;
+
+    outputValidation(nama, tglLahir, gender, pesan);
+    return false;
+}
+
+function outputValidation(nama, tglLahir, gender, pesan) {
+    document.getElementById("output-nama").innerHTML = nama;
+    document.getElementById("output-lahir").innerHTML = tglLahir;
+    document.getElementById("output-kelamin").innerHTML = gender;
+    document.getElementById("output-pesan").innerHTML = pesan;
+}
