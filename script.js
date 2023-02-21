b3c6f2f384bb30a71c1e6b77f2cecd7deb48aeec
Nabil Alfaatir <nabil_alfaatir09@yahoo.com>
2023-02-21 14:18:29 +0700

    clear my projek

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function validation() {
     const gender = document.forms["form-contact"]["gender"].value;
     const pesan = document.forms["form-contact"]["pesan"].value;
 
+    outputNama(nama);
     outputValidation(nama, tglLahir, gender, pesan);
     return false;
 }
@@ -25,4 +26,8 @@ function outputValidation(nama, tglLahir, gender, pesan) {
     document.getElementById("output-lahir").innerHTML = tglLahir;
     document.getElementById("output-kelamin").innerHTML = gender;
     document.getElementById("output-pesan").innerHTML = pesan;
+}
+
+function outputNama(nama) {
+    document.querySelector(".nama").innerHTML = nama;
 }
